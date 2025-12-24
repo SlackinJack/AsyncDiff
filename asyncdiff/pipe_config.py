da09159f52bf5856132553b002f16c39f29b909f
Jacky Guo <41076523+SlackinJack@users.noreply.github.com>
2025-12-24 00:07:43 -0700

    support zimage, remove unused

--- asyncdiff/pipe_config.py
+++ asyncdiff/pipe_config.py
@@ -1,13 +1,12 @@
 def splite_model(pipe, pipe_id, n):
-    if pipe_id in ["flux", "sd3"]:
+    if pipe_id in ["flux", "sd3", "zimage"]:
         transformer = pipe.transformer
     else:
         unet = pipe.unet
 
     if pipe_id == "svd":
         if n == 1:
-            return [
-                (
+            return [(
                 unet.conv_in,
                 *unet.down_blocks,
                 unet.mid_block,
@@ -16,53 +15,43 @@ def splite_model(pipe, pipe_id, n):
                 *unet.up_blocks[2:],
                 unet.conv_norm_out,
                 unet.conv_out
-            )
-            ]
+            )]
         elif n == 2:
-            return [
-                (
+            return [(
                 unet.conv_in,
                 *unet.down_blocks,
                 unet.mid_block,
                 unet.up_blocks[0],
                 unet.up_blocks[1],
-            ),
-                (
+            ), (
                 *unet.up_blocks[2:],
                 unet.conv_norm_out,
                 unet.conv_out
-            )
-            ]
+            )]
         elif n == 3:
-            return [
-                (
+            return [(
                 unet.conv_in,
                 unet.down_blocks[0],
                 unet.down_blocks[1],
                 unet.down_blocks[2],
-            ),
-                (
+            ), (
                 unet.down_blocks[3],
                 unet.mid_block,
                 unet.up_blocks[0],
                 unet.up_blocks[1],
                 unet.up_blocks[2],
-            ),
-                (
+            ), (
                 unet.up_blocks[3],
                 unet.conv_norm_out,
                 unet.conv_out
-            )
-            ]
+            )]
         elif n == 4:
-            return [
-                (
+            return [(
                 unet.down_blocks[1].resnets[0],
                 unet.down_blocks[1].attentions[0],
                 unet.conv_in,
                 unet.down_blocks[0],
-            ),
-                (
+            ), (
                 unet.down_blocks[1].resnets[1],
                 unet.down_blocks[1].attentions[1],
                 *unet.down_blocks[1].downsamplers,
@@ -70,21 +59,18 @@ def splite_model(pipe, pipe_id, n):
                 unet.mid_block,
                 unet.up_blocks[0],
                 unet.up_blocks[1],
-            ),
-                (
+            ), (
                 unet.up_blocks[2],
                 unet.up_blocks[3].resnets[0],
                 unet.up_blocks[3].attentions[0],
-            ),
-            (
+            ), (
                 unet.up_blocks[3].resnets[1],
                 unet.up_blocks[3].attentions[1],
                 unet.up_blocks[3].resnets[2],
                 unet.up_blocks[3].attentions[2],
                 unet.conv_norm_out,
                 unet.conv_out
-            )
-            ]
+            )]
         else:
             raise NotImplementedError
     elif pipe_id == "sd2":
@@ -446,11 +432,9 @@ def splite_model(pipe, pipe_id, n):
                 unet.mid_block,
                 unet.up_blocks[0],
                 unet.up_blocks[1],
-            ),
-                (
+            ), (
                 unet.up_blocks[2],
-            ),
-                (
+            ), (
                 unet.up_blocks[3],
                 unet.conv_norm_out,
                 unet.conv_out
@@ -490,14 +474,13 @@ def splite_model(pipe, pipe_id, n):
                 *transformer.transformer_blocks[6:12],
             ), (
                 *transformer.transformer_blocks[12:18],
-            ),(
+            ), (
                 *transformer.transformer_blocks[18:24],
                 transformer.norm_out,
                 transformer.proj_out
             )]
         else:
             raise NotImplementedError
-    # TODO: test
     elif pipe_id == "flux":
         if n == 1:
             return [(
@@ -531,10 +514,54 @@ def splite_model(pipe, pipe_id, n):
                 *transformer.single_transformer_blocks[0:16],
             ), (
                 *transformer.single_transformer_blocks[16:32],
-            ),(
+            ), (
                 *transformer.single_transformer_blocks[32:38],
                 transformer.norm_out,
                 transformer.proj_out
+            )]
+        else:
+            raise NotImplementedError
+    # TODO: test
+    elif pipe_id == "zimage":
+        if n == 1:
+            return [(
+                *transformer.noise_refiner[0:2],
+                *transformer.context_refiner[0:2],
+                *transformer.layers[0:30],
+                transformer.all_final_layer,
+            )]
+        elif n == 2:
+            return [(
+                *transformer.noise_refiner[0:2],
+                *transformer.context_refiner[0:2],
+                *transformer.layers[0:15],
+            ), (
+                *transformer.layers[15:30],
+                transformer.all_final_layer,
+            )]
+        elif n == 3:
+            return [(
+                *transformer.noise_refiner[0:2],
+                *transformer.context_refiner[0:2],
+                *transformer.layers[0:10],
+            ), (
+                *transformer.layers[10:23],
+            ), (
+                *transformer.layers[23:30],
+                transformer.all_final_layer,
+            )]
+        elif n == 4:
+            return [(
+                *transformer.noise_refiner[0:2],
+                *transformer.context_refiner[0:2],
+                *transformer.layers[0:6],
+            ), (
+                *transformer.layers[6:15],
+            ), (
+                *transformer.layers[15:25],
+            ), (
+                *transformer.layers[25:30],
+                transformer.all_final_layer,
             )]
         else:
             raise NotImplementedError
